5fc15f1e9fccbaedcc03cd7a39230dd1a18bb971
Kinga Paszkiewicz <kingaa.kulig@gmail.com>
2021-03-31 17:21:07 +0200

    Stworzenie klasy Product

---
+++ src/main/java/pl/kinga/produkty/Product.java
@@ -0,0 +1,48 @@
+package pl.kinga.produkty;
+
+public class Product {
+    private String name;
+    private String price;
+    private String category;
+
+    public Product(String name, String price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
+
+
