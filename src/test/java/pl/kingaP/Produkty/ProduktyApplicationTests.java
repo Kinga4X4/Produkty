f654d9f21374c610ded1c5b5f2daebe484278ceb
Kinga Paszkiewicz <kingaa.kulig@gmail.com>
2021-03-31 15:04:38 +0200

    Rozwiązanie zadania

---
+++ src/test/java/pl/kingaP/Produkty/ProduktyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kingaP.Produkty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
